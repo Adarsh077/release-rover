ba36620aa62e9a3f4af51aa122f10276c12ce9ee
Adarsh Senghani <adarshco077@gmail.com>
2024-01-20 22:30:03 +0530

    feat(cron): added cron job

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Client, Events, GatewayIntentBits } = require("discord.js");
+const schedule = require("node-schedule");
 const { token } = require("./config.json");
 const registerCommands = require("./register-commands");
 const checkForNewEpisodes = require("./check-for-new-episodes");
@@ -18,6 +19,10 @@ client.once(Events.ClientReady, (readyClient) => {
   // client.channels.cache
   //   .get("1197964105819103397")
   //   .send({ content: `${userMention("945268596714663976")} hello!` });
+  checkForNewEpisodes(client);
+});
+
+schedule.scheduleJob("0 * * * *", function () {
   checkForNewEpisodes(client);
 });
 
